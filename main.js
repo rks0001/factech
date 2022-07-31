91bbce78caf550514504ad9f4d786a1aab1010d8
Ranjit Kalita <ranjitkalita499@gmail.com>
2022-08-01 00:00:19 +0530

    changes

---
+++ main.js
@@ -0,0 +1,8 @@
+// const api_url ='https://codeforces.com/api/user.info?handles=DmitriyH;Fefer_Ivan'
+//     async function getData() {
+//         const response = await fetch(api_url);
+//         const data = await response.json();
+//         console.log(data);
+//     }
+
+//     getData();
